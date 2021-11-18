c82b07ebdf0b42734683c3954516a6f35bdd69e7
elton-tanaka <elton.tanaka@univem.edu.br>
2021-11-17 22:53:13 -0300

    fixed schema and added collection

--- live/graphql/backend/src/resolvers.js
+++ live/graphql/backend/src/resolvers.js
@@ -8,5 +8,7 @@ module.exports = {
 
     Mutation: {
         createStudent: (_, { name, course, semester, ra, cpf, city }) => Student.create({ name, course, semester, ra, cpf, city }),
+        deleteStudentById: (_, { id }) => Student.findByIdAndDelete(id),
+        editStudentById: (_, { id, name, course, semester, ra, cpf, city }) => Student.findByIdAndUpdate(id, { name, course, semester, ra, cpf, city })
     },
 };
